9481058e3d9cf1de11cfb01be4d33069dd6d79e6
Jared Gain <jlgain7@gmail.com>
2020-01-23 23:05:39 -0600

    Updated JS coding portion

--- js/script.js
+++ js/script.js
@@ -4,60 +4,115 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+   Global variables for DOM elements to reference and/or manipulate. 
 ***/
-const studentList = document.querySelectorAll('ul li');
+const studentList = document.getElementsByClassName('.student-item cf');
 const studentsPerPage = 10;
-
+const toggleSearch = document.querySelector('.search');
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
+   `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
 ***/
-function showPage(studentList, page)
+const showPage = (studentList, page) =>
 {
-   // Declare variables start and end index for list of items shown on page
+   // Declare local variables start and end index for list of items shown on page
    let startIndex = (page * studentsPerPage) - studentsPerPage;
    let endIndex = page * studentsPerPage;
 
+   // Use a for loop to loop over the list items 
    for (let i = 0; i <= studentList.length; i++)
    {  
-      if (studentList[i] >= startIndex && studentList[i] < endIndex)
+      // If statement to display list items dependent on the index and page/button selected
+      if (i >= startIndex && i < endIndex)
       {
-         studentList[i].style.display = '';
+         studentList[i].style.display = "block";
       }
       else
       {
-         studentList[i].style.display = 'none';
+         studentList[i].style.display = "none";
       }
    }
 }
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+   `appendPageLinks` function to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 const appendPageLinks = (list) =>
 {
+   // Declare local variables 
+   let numberOfPages = Math.ceil(studentList.length / studentsPerPage);
+   let pageDiv = document.querySelector('.page');
+   let paginationDiv = document.createElement('div');
+   let paginationUl = document.createElement('ul');   
 
+   // Assign pagination div element a new class name 
+   paginationDiv.className = 'pagination';
+
+   // Append pagination div & ul elements to pageDiv and paginationDiv
+   pageDiv.appendChild(paginationDiv);
+   paginationDiv.appendChild(paginationUl);
+
+   // Use for loop to loop through the number of pages to add a li and a tag
+   for (let i = 0; i < numberOfPages; i++)
+   {
+      // Declare nexted variables for li & a elements
+      let paginationLi = document.createElement('li');
+      let paginationA = document.createElement('a');
+
+      // Append paginationLi & paginationA elements
+      paginationUl.appendChild(paginationLi);
+      paginationLi.appendChild(paginationA);
+
+      // Set anchor element href attribute to # & set text to the page number for each link
+      paginationA.href = "#";
+      paginationA.textContent = i + 1;
+
+      // If statment to add active class name to first pagination link
+      if (i === 0)
+      {  
+         paginationA.setAttribute('class', 'active');
+      }
+   }
+
+   // Declare a DOM element for the anchor element - Found this in ES2015 video for loops
+   const pageClicked = document.getElementsByTagName('a');
+
+   // Use for loop to loop through all page link and match the page with the students in the list
+   for (let i = 0; i < pageClicked.length; i++)
+   {
+      // Add event listener when acnhor/page/button is selected
+      paginationA[i].addEventListener('click', (event) =>
+      {
+         // Remove active class from previous clicked link
+         let previousPage = document.querySelector('.active');
+         previousPage.className = '';
+
+         // Add active class to current link
+         let currentPage = event.target;
+         currentPage.className = 'active';
+
+         // Call showPage function passing 2 parms
+         showPage(studentList, currentPage);
+      });
+   }
 }
 
+/***
+    Toggle Search function is an event listener to show student entered in search 
+ ***/
+// toggleSearch.addEventListener('click', () =>
+// {     
+//   if (listDiv.style.display == 'none') 
+//   {
+//     listDiv.style.display = 'block'; 
+//   }
+//   else
+//   { 
+//     toggleList.textContent = 'Show List';
+//     listDiv.style.display = 'none'; 
+//   }
+// });
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
